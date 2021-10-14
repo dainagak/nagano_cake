18c86799d07914cce997e85f0eaeb5e44b2f7142
EC2 Default User <ec2-user@ip-172-31-45-55.ec2.internal>
2021-10-14 15:42:18 +0000

    [Add]admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
